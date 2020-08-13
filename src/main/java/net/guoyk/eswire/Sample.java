56c8370616579622be84bf2e9a16e0688a70085c
Guo Y.K <hi@guoyk.net>
2020-08-13 21:08:27 +0800

    Update Sample.java

--- src/main/java/net/guoyk/eswire/Sample.java
+++ src/main/java/net/guoyk/eswire/Sample.java
@@ -3,12 +3,12 @@ package net.guoyk.eswire;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
-import java.net.UnknownHostException;
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 public class Sample {
 
-    public static void main(String[] args) throws UnknownHostException, ExecutionException, InterruptedException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         Yaml yaml = new Yaml(new Constructor(ElasticWireOptions.class));
         ElasticWireOptions options = yaml.load(Sample.class.getClassLoader().getResourceAsStream("net.guoyk.eswire.sample.yml"));
         ElasticWire elasticWire = new ElasticWire(options);
